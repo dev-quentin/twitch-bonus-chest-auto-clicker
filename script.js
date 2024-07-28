e7fe6beec7fe728d6efe47961ce8e0d1aaa372c6
Quentin <53433390+dev-quentin@users.noreply.github.com>
2024-07-28 10:14:52 +0200

    Add: logo & random click timeout

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name        Twitch Bonus Chest Auto Clicker
-// @namespace   Violentmonkey Scripts
+// @icon        https://raw.githubusercontent.com/dev-quentin/twitch-bonus-chest-auto-clicker/main/logo.jpg
+// @namespace   0f4494b0ce95bc04feac86a004f7fdd1
 // @match       https://www.twitch.tv/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      https://github.com/dev-quentin
 // ==/UserScript==
 
@@ -11,24 +12,26 @@ const TIME = 1000;
 const SELECTOR = 'path[d="M2 6v11h16V6a3 3 0 0 0-3-3H5a3 3 0 0 0-3 3Zm10-1H8v2h4V5Zm2 0v2h2V6a1 1 0 0 0-1-1h-1ZM4 9v6h12V9H4Zm1-4h1v2H4V6a1 1 0 0 1 1-1Z"]';
 
 function getParent(node) {
-    return node.parentNode ?? null;
+  return node.parentNode ?? null;
 }
 
 setInterval(() => {
-    let domNode = document.querySelector(SELECTOR);
+  let domNode = document.querySelector(SELECTOR);
 
-    if (!domNode) {
-        return;
-    }
+  if (!domNode) {
+    return;
+  }
 
-    while (domNode.nodeName != "BUTTON") {
-        domNode = getParent(domNode);
-        if (domNode === null) {
-            break;
-        }
+  while (domNode.nodeName != "BUTTON") {
+    domNode = getParent(domNode);
+    if (domNode === null) {
+      break;
     }
+  }
 
-    if (domNode) {
-        domNode.click();
-    }
+  if (domNode) {
+    setTimeout(() => {
+      domNode.click();
+    }, Math.floor(Math.random() * 3) + 1);
+  }
 }, TIME);
